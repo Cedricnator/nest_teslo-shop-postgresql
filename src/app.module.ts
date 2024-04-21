e2509ed9524230d098d2040349fcf01d26b44854
Cedric Kirmayr <Cedric.kirmayr@gmail.com>
2024-04-21 15:26:11 -0400

    feat():Add PostgreSQL configuration and connection in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: +process.env.DB_PORT | 9432,
+      database: 'postgres',
+      username: 'postgres',
+      password: 'postgres',
+      autoLoadEntities: true,
+      synchronize: true // NEVER USE IN PRODUCTION,
+    })
+    
+  ],
   controllers: [],
   providers: [],
 })
